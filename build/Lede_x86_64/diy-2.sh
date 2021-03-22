7f85fbafab25fc07c45d0cb0a3662d5297e2dded
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-03-22 14:06:23 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
@@ -15,7 +16,7 @@ sed -i '/exit 0/i\uci set network.lan.dns='223.5.5.5 114.114.114.115'' $ZZZ
 # 取消LAN口内置IPV6使用
 sed -i '/exit 0/i\uci set network.lan.delegate='0'' $ZZZ
 # 以上修改没全部删除,勿删除此处
-sed -i '/exit 0/i\uci commit network'' $ZZZ
+sed -i '/exit 0/i\uci commit network' $ZZZ
 
 # 修改默认主题为argon
 sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon' $ZZZ
@@ -23,8 +24,11 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon' $Z
 # 修改主机名称为OpenWrt-123
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' $ZZZ
 
-# 设置密码为空
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ
+# 版本号里显示一个自己的名字（把 ${Author} 改成你自己名字就行了，不改的话，默认使用你帐号）
+sed -i "s/OpenWrt /Compiled by ${Author} on $(TZ=UTC-8 date "+%Y/%m/%d") @ OpenWrt /g" $ZZZ
 
-# 修改内核版本
+# 设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+
+# 修改内核版本为4.19
 #sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile
