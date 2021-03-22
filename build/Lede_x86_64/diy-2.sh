814b889b0c92fc7d85f9ff9036cb143d97b293ac
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-03-22 13:41:05 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -2,6 +2,20 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
+sed -i '/exit 0/i\uci set network.lan.ipaddr='192.168.2.2'' $ZZZ              # IPv4 地址
+sed -i '/exit 0/i\uci set network.lan.netmask='255.255.255.0'' $ZZZ           # IPv4 子网掩码
+sed -i '/exit 0/i\uci set network.lan.gateway='192.168.2.1'' $ZZZ             # IPv4 网关
+sed -i '/exit 0/i\uci set network.lan.broadcast='192.168.2.255'' $ZZZ         # IPv4 广播
+sed -i '/exit 0/i\uci set network.lan.dns='223.5.5.5 114.114.114.115'' $ZZZ   # DNS 多个要空格分开
+sed -i '/exit 0/i\uci set network.lan.delegate='0'' $ZZZ                      # 取消LAN口内置IPV6使用
+sed -i '/exit 0/i\uci commit network'' $ZZZ                                   # 以上修改没全部删除,勿删除此处
+
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon' $ZZZ    # 修改默认主题为argon
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' $ZZZ   # 修改主机名称为OpenWrt-123
+
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ  #设置密码为空
+
+
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
