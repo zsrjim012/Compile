784be47351b477097913f9a1532a6f94617e334f
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-03-22 15:15:10 +0800

    Update diy-2.sh

--- build/Lienol_x86_64/diy-2.sh
+++ build/Lienol_x86_64/diy-2.sh
@@ -1,19 +1,1 @@
 #!/bin/bash
-ZZZ="package/default-settings/files/zzz-default-settings"
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-
-# IPv4 地址
-sed -i "/exit 0/i\uci set network.lan.ipaddr='192.168.2.2'" $ZZZ
-
-# 修改默认主题为argon
-sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon" $ZZZ
-
-# 修改主机名称为OpenWrt-123
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'" $ZZZ
-
-# 版本号里显示一个自己的名字（把 ${Author} 改成你自己名字就行了，不改的话，默认使用你帐号）
-sed -i "s/OpenWrt /Compiled by ${Author} on $(TZ=UTC-8 date "+%Y/%m/%d") @ OpenWrt /g" $ZZZ
-
-# 修改内核版本为4.19
-#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
